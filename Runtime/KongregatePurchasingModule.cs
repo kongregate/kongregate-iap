acd57a95ec84391b8f2f16e7b82614573b55be50
David LeGare <dlegare.1001@gmail.com>
2019-04-17 16:30:58 -0500

    Add purchasing module

---
+++ Runtime/KongregatePurchasingModule.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Purchasing.Extension;
+
+namespace Kongregate.Purchasing
+{
+    public class KongregatePurchasingModule : AbstractPurchasingModule
+    {
+        public static KongregatePurchasingModule Instance()
+        {
+            return new KongregatePurchasingModule();
+        }
+
+        public override void Configure()
+        {
+            RegisterStore(
+                KongregateStore.STORE_NAME,
+                Application.platform == RuntimePlatform.WebGLPlayer ? new KongregateStore() : null);
+        }
+
+        // NOTE: Make the default constructor private so that users must use the
+        // Instance() function.
+        private KongregatePurchasingModule() { }
+    }
+}
