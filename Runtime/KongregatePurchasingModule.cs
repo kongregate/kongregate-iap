d8c2f3b6fbb520ef4a7f832cc1ba7388029ebdba
alejandro <alejandro@dedalord.com>
2019-08-07 17:49:35 -0300

    Throw an exception if the user tries to register the store before initializing the web API

--- Runtime/KongregatePurchasingModule.cs
+++ Runtime/KongregatePurchasingModule.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Purchasing.Extension;
 
 namespace Kongregate.Purchasing
@@ -12,6 +13,11 @@ namespace Kongregate.Purchasing
 
         public override void Configure()
         {
+            if (KongregateWeb.Status == KongregateWeb.ApiStatus.Uninitialized)
+            {
+                throw new Exception($"You must first intialize {typeof(KongregateWeb).Name} before trying to register the store.");
+            }
+
             RegisterStore(
                 KongregateStore.STORE_NAME,
                 (Application.platform == RuntimePlatform.WebGLPlayer && KongregateWeb.Status != KongregateWeb.ApiStatus.Unavailable) ? new KongregateStore() : null);
